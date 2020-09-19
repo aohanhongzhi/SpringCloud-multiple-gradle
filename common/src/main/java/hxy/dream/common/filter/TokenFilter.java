2cfd50cfd8b5f128c49e0b065a4a581b65bcf32e
aohanhongzhi <aohanhongzhi@qq.com>
2020-09-19 23:02:58 +0800

    add filter

---
+++ common/src/main/java/hxy/dream/common/filter/TokenFilter.java
@@ -0,0 +1,42 @@
+package hxy.dream.common.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import java.io.IOException;
+
+@Order(1)
+@WebFilter(filterName = "TokenFilter", urlPatterns = "/*" , initParams = {
+        @WebInitParam(name = "URL", value = "http://localhost:8080")})
+public class TokenFilter implements Filter {
+
+    Logger logger = LoggerFactory.getLogger(TokenFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        logger.info("\n====>TokenFilter初始化配置信息[{}]", filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        logger.info("\n====>TokenFilter过滤器使用[{}]", chain);
+
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        logger.info("\n====>TokenFilter销毁");
+
+    }
+}
