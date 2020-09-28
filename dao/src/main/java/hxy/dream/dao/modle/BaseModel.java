9b7e6aace353449136a35c7db83a79876640d26c
傲寒宏志 <aohanhongzhi@qq.com>
2020-09-28 11:12:31 +0800

    逻辑删除

--- dao/src/main/java/hxy/dream/dao/modle/BaseModel.java
+++ dao/src/main/java/hxy/dream/dao/modle/BaseModel.java
@@ -14,12 +14,41 @@ import java.time.LocalDateTime;
  */
 public class BaseModel<T extends Model<?>> extends Model<T> {
 
-//    @TableField(fill = FieldFill.INSERT)
+    //    @TableField(fill = FieldFill.INSERT)
 //    private String creator;
     @TableField(fill = FieldFill.INSERT)
     private LocalDateTime createTime;
-//    @TableField(fill = FieldFill.UPDATE)
+    //    @TableField(fill = FieldFill.UPDATE)
 //    private String updater;
     @TableField(fill = FieldFill.UPDATE)
     private LocalDateTime updateTime;
+
+    /**
+     * 逻辑删除必须,使用int因为有初始值0
+     */
+    private int deleted;
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+
+    public LocalDateTime getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(LocalDateTime updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public int getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(int deleted) {
+        this.deleted = deleted;
+    }
 }
