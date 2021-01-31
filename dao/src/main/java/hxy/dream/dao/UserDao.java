fa7fc002ddfc043aab13a2560b29778fe518e35d
aohanhongzhi <aohanhongzhi@qq.com>
2021-01-31 14:13:24 +0800

    数据库层的封装

---
+++ dao/src/main/java/hxy/dream/dao/UserDao.java
@@ -0,0 +1,25 @@
+package hxy.dream.dao;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import hxy.dream.dao.mapper.UserMapper;
+import hxy.dream.dao.modle.UserModel;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author iris
+ */
+@Component
+public class UserDao {
+
+    @Resource
+    UserMapper userMapper;
+
+    public List<UserModel> selectByName(String name) {
+        QueryWrapper<UserModel> objectQueryWrapper = new QueryWrapper<UserModel>();
+        objectQueryWrapper.eq("name", name);
+        return userMapper.selectList(objectQueryWrapper);
+    }
+}
--- dao/src/main/java/hxy/dream/dao/mapper/UserMapper.java
+++ dao/src/main/java/hxy/dream/dao/mapper/UserMapper.java
@@ -3,5 +3,8 @@ package hxy.dream.dao.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import hxy.dream.dao.modle.UserModel;
 
+/**
+ * @author iris
+ */
 public interface UserMapper extends BaseMapper<UserModel> {
 }
