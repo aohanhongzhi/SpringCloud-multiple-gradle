5e27a180e0ae82fe18de68a441f8104eca57883d
aohanhongzhi <aohanhongzhi@qq.com>
2021-11-29 15:07:36 +0800

    Long

--- app/src/main/java/hxy/dream/app/controller/LongController.java
+++ app/src/main/java/hxy/dream/app/controller/LongController.java
@@ -1,5 +1,6 @@
 package hxy.dream.app.controller;
 
+import hxy.dream.common.serializer.BaseLongSerializer;
 import hxy.dream.entity.vo.BaseResponseVO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import java.util.HashMap;
 /**
  * @author eric
  * @date 2021/8/13
+ * @see BaseLongSerializer
  */
 @RestController
 public class LongController {
