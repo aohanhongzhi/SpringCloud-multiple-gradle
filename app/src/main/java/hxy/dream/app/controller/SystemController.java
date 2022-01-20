d1ab4006a3f2f90e229367320d671769bf12c4b2
aohanhongzhi <aohanhongzhi@qq.com>
2022-01-21 00:03:16 +0800

    并发测试

--- app/src/main/java/hxy/dream/app/controller/SystemController.java
+++ app/src/main/java/hxy/dream/app/controller/SystemController.java
@@ -1,6 +1,7 @@
 package hxy.dream.app.controller;
 
 import hxy.dream.entity.vo.BaseResponseVO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @description
  * @date 2021/8/29
  */
+@Slf4j
 @RestController
 public class SystemController {
     @GetMapping("/")
     public BaseResponseVO index(){
+
+//        try {
+//            Thread.sleep(10);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        log.info("{}",Thread.currentThread().getName());
+
         return BaseResponseVO.success("SpringBoot");
     }
 }
