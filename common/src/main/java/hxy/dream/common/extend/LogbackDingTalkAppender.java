6ff5ebd0ec417889deeae7954efd7d8dc623808c
aohanhongzhi <aohanhongzhi@qq.com>
2022-03-05 09:31:58 +0800

    本地环境变量不需要处理钉钉

--- common/src/main/java/hxy/dream/common/extend/LogbackDingTalkAppender.java
+++ common/src/main/java/hxy/dream/common/extend/LogbackDingTalkAppender.java
@@ -4,9 +4,12 @@ import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.UnsynchronizedAppenderBase;
 import com.ejlchina.okhttps.HTTP;
+import com.ejlchina.okhttps.OkHttps;
 import com.ejlchina.okhttps.jackson.JacksonMsgConvertor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -16,7 +19,7 @@ import java.util.concurrent.ConcurrentHashMap;
  * @description 错误消息发送到钉钉上
  * @date 2021/10/28
  */
-
+@Component
 public class LogbackDingTalkAppender extends UnsynchronizedAppenderBase<ILoggingEvent> {
 
     private static final Logger log = LoggerFactory.getLogger(LogbackDingTalkAppender.class);
@@ -25,9 +28,19 @@ public class LogbackDingTalkAppender extends UnsynchronizedAppenderBase<ILogging
             .addMsgConvertor(new JacksonMsgConvertor())
             .build();
 
+    @Value("spring.profiles.active")
+    String activeProfile;
+
     @Override
     protected void append(ILoggingEvent eventObject) {
         Level level = eventObject.getLevel();
+
+        if (activeProfile == null || activeProfile.contains("dev") || activeProfile.contains("test")) {
+            if (level.toInt() == Level.ERROR_INT) {
+                log.debug("\n====>当前是本地测试环境，错误信息不通知钉钉");
+            }
+            return;
+        }
         switch (level.toInt()) {
             case Level.ERROR_INT:
                 // 发送到钉钉
@@ -39,8 +52,8 @@ public class LogbackDingTalkAppender extends UnsynchronizedAppenderBase<ILogging
                 String msg = String.format("时间：%s,级别:%s,原因%s", timeStamp, eventObject.getLevel(), eventObject.getFormattedMessage());
                 postBody.put("title", loggerName);
                 postBody.put("msg", msg);
-//                String s = http.async("api/dingtalk/v1/notice").setBodyPara(postBody).bodyType(OkHttps.JSON).post().getResult().getBody().toString();
-//                log.debug("钉钉返回信息{}", s);
+                String s = http.async("api/dingtalk/v1/notice").setBodyPara(postBody).bodyType(OkHttps.JSON).post().getResult().getBody().toString();
+                log.debug("钉钉返回信息{}", s);
                 break;
             case Level.WARN_INT:
             case Level.INFO_INT:
