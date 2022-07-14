2102c6bb5ade3455c40830b28c118ac1fc907e32
HouXiaoyi <xiaoyi.hou@cn.bosch.com>
2022-07-14 18:05:46 +0800

    日志配置

--- app/src/main/java/hxy/dream/app/Application.java
+++ app/src/main/java/hxy/dream/app/Application.java
@@ -24,6 +24,8 @@ public class Application {
 
     public static void main(String[] args) {
         log.info("当前CPU核心={}，{}是否为守护线程={}",Runtime.getRuntime().availableProcessors(), Thread.currentThread().getName(), Thread.currentThread().isDaemon());
+        log.info("\n<============ \uD83D\uDE80  JAVA版本:{}  CPU核心数:{}  \uD83D\uDE80 ============>", System.getProperty("java.version"), Runtime.getRuntime().availableProcessors());
+
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
         log.trace("方便打断点");
     }
