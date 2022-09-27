52f0b627127fda89a6dbd8d3017f8a5a4fee0a5e
HouXiaoyi <xiaoyi.hou@cn.bosch.com>
2022-09-27 09:42:23 +0800

    test

--- app/src/test/java/hxy/dream/BaseTest.java
+++ app/src/test/java/hxy/dream/BaseTest.java
@@ -4,8 +4,10 @@ import hxy.dream.app.Application;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest(classes = Application.class)
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
 public abstract class BaseTest {
 }
