f69b621bfeaa267c67e5952cd48c008bf248429f
HouXiaoyi <xiaoyi.hou@cn.bosch.com>
2022-11-25 11:15:21 +0800

    Thread里面是有一个ThreadLocalMap的

--- app/src/main/java/hxy/dream/app/controller/ThreadLocalController.java
+++ app/src/main/java/hxy/dream/app/controller/ThreadLocalController.java
@@ -18,7 +18,9 @@ import java.util.Map;
 @RestController
 public class ThreadLocalController {
 
+    // Thread里面有一个属性 ThreadLocal.ThreadLocalMap ，下面两个ThreadLocal，就会作为key存储在Map当中
     private static final ThreadLocal<Integer> currentUser = ThreadLocal.withInitial(() -> null);
+    private static final ThreadLocal<Long> timeCounter = ThreadLocal.withInitial(() -> null);
 
     @GetMapping("wrong")
     public Map wrong(@RequestParam("userId") Integer userId) {
@@ -26,8 +28,9 @@ public class ThreadLocalController {
         String before = Thread.currentThread().getName() + ":" + currentUser.get();
         //设置用户信息到ThreadLocal
         currentUser.set(userId);
+        timeCounter.set(System.currentTimeMillis());
         //设置用户信息之后再查询一次ThreadLocal中的用户信息
-        String after = Thread.currentThread().getName() + ":" + currentUser.get();
+        String after = Thread.currentThread().getName() + ":" + currentUser.get() + ":" + timeCounter.get();
         //汇总输出两次查询结果
         Map result = new HashMap();
         result.put("before", before);
