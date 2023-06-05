38e46c7fc5cfe946dbab287aa40b8206cdf712ed
捕风的逍遥侯 <aohanhongzhi@qq.com>
2023-06-05 21:38:01 +0800

    1

--- app/src/test/java/hxy/dream/dao/mapper/UserMapperTest.java
+++ app/src/test/java/hxy/dream/dao/mapper/UserMapperTest.java
@@ -1,5 +1,6 @@
 package hxy.dream.dao.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import hxy.dream.BaseTest;
 import hxy.dream.dao.model.UserModel;
 import jakarta.annotation.Resource;
@@ -26,11 +27,27 @@ public class UserMapperTest extends BaseTest {
         UserModel userModel = new UserModel();
         userModel.setId(1);
         userModel.setAge(23);
-        userModel.setName(null);
+        userModel.setName(null); // mybatis-plus是默认不更新 null的属性
 //        https://blog.csdn.net/konnysnow/article/details/121421210
 //        当然也可以用 lambdaUpdateWrapper.set(userModel::getName, null);
         int i = userMapper.updateById(userModel);
         log.info("Updated {}", i);
     }
+
+    @Test
+    public void testUpdateSetNull() {
+        UserModel userModel = new UserModel();
+        userModel.setId(1);
+        userModel.setAge(231);
+
+        LambdaUpdateWrapper<UserModel> lambdaUpdateWrapper = new LambdaUpdateWrapper<UserModel>();
+        lambdaUpdateWrapper.set(UserModel::getName, null); // 强制修改数据库为null
+        lambdaUpdateWrapper.eq(UserModel::getId, userModel.getId());
+
+        int update = userMapper.update(userModel, lambdaUpdateWrapper);
+        log.info("Updated user{}", update);
+
+    }
+
 
 }
