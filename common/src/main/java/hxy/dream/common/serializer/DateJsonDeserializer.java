852da1ad5cfaba612edee029461a2f9dd0f79587
捕风的逍遥侯 <aohanhongzhi@qq.com>
2023-07-10 17:12:25 +0800

    兼容elmentUi的前端时间格式

--- common/src/main/java/hxy/dream/common/serializer/DateJsonDeserializer.java
+++ common/src/main/java/hxy/dream/common/serializer/DateJsonDeserializer.java
@@ -10,13 +10,20 @@ import java.util.Date;
 
 public class DateJsonDeserializer extends JsonDeserializer<Date> {
     public static final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    public static final SimpleDateFormat format2 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
 
     @Override
     public Date deserialize(com.fasterxml.jackson.core.JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, com.fasterxml.jackson.core.JsonProcessingException {
 
         try {
-            if (jsonParser != null && StringUtils.hasText(jsonParser.getText())) {
-                return format.parse(jsonParser.getText());
+            String text = jsonParser.getText();
+            if (jsonParser != null && StringUtils.hasText(text)) {
+                if (text.contains("T")) {
+                    // element ui
+                    return format2.parse(text);
+                } else {
+                    return format.parse(text);
+                }
             } else {
                 return null;
             }
