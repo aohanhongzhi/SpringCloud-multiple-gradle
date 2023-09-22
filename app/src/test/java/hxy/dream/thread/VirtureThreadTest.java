b7c03c6d4863a2de270fe6e2ea01ded1111b59af
捕风的逍遥侯 <aohanhongzhi@qq.com>
2023-09-22 13:57:28 +0800

    虚拟线程

---
+++ app/src/test/java/hxy/dream/thread/VirtureThreadTest.java
@@ -0,0 +1,32 @@
+package hxy.dream.thread;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+/**
+ * @author eric
+ * @description
+ * @date 2023/9/22
+ */
+public class VirtureThreadTest {
+
+    private static final Logger log = LoggerFactory.getLogger(VirtureThreadTest.class);
+
+
+    public static void main(String[] args){
+        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            IntStream.range(0, 10_000).forEach(i -> {
+                executor.submit(() -> {
+                    log.info("Thread " + i + "");
+                    Thread.sleep(Duration.ofSeconds(1));
+                    return i;
+                });
+            });
+        }  // executor.close() is called implicitly, and waits
+    }
+
+}
