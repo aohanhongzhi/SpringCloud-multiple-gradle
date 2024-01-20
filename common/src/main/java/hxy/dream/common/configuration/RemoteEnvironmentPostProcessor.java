b969e195cc3b8373470d969f2432fa4b6cf9723f
捕风的逍遥侯 <aohanhongzhi@qq.com>
2024-01-20 13:53:05 +0800

    可以需要特定环境变量

--- common/src/main/java/hxy/dream/common/configuration/RemoteEnvironmentPostProcessor.java
+++ common/src/main/java/hxy/dream/common/configuration/RemoteEnvironmentPostProcessor.java
@@ -12,6 +12,7 @@ import org.springframework.core.env.PropertiesPropertySource;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -31,36 +32,44 @@ public class RemoteEnvironmentPostProcessor implements EnvironmentPostProcessor 
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
         String currentProfile = environment.getProperty("spring.profiles.active");
-        if (currentProfile == null || currentProfile.equalsIgnoreCase("dev")) {
-            log.warn("本地调试，直接忽略读取本地配置文件信息");
+        if (currentProfile == null) {
+            // 这里需要带特定环境变量的配置文件，即没有 dev,beta,prod都是可以的
+            Map<String, Object> systemProperties = environment.getSystemProperties();
+            systemProperties.put("key", "value");
         } else {
-            final Properties properties = new Properties();
+            if (currentProfile.equalsIgnoreCase("dev")) {
+                log.warn("本地调试，直接忽略读取本地配置文件信息");
+            } else {
+                final Properties properties = new Properties();
 
-            // 读取配置文件，从配置文件中加载这个变量。
-            String database = System.getProperty("user.home") + "/.config/eric-config/database.json";
-            ObjectMapper objectMapper = new ObjectMapper();
-            try {
-                JsonNode jsonObject = objectMapper.readTree(new File(database));
-                String databaseUsername = jsonObject.get("spring.datasource.username").textValue();
-                String databaseUrl = jsonObject.get("spring.datasource.url").textValue();
-                String password = jsonObject.get("spring.datasource.password").textValue();
-                properties.put("spring.datasource.url", databaseUrl);
-                properties.put("spring.datasource.username", databaseUsername);
-                properties.put("spring.datasource.password", password);
-            } catch (JsonProcessingException e) {
-                log.error("{}", e.getMessage(), e);
-            } catch (IOException e) {
-                log.error("{}", e.getMessage(), e);
-            }
+                // 读取配置文件，从配置文件中加载这个变量。
+                String database = System.getProperty("user.home") + "/.config/eric-config/database.json";
+                ObjectMapper objectMapper = new ObjectMapper();
+                try {
+                    JsonNode jsonObject = objectMapper.readTree(new File(database));
+                    String databaseUsername = jsonObject.get("spring.datasource.username").textValue();
+                    String databaseUrl = jsonObject.get("spring.datasource.url").textValue();
+                    String password = jsonObject.get("spring.datasource.password").textValue();
+                    properties.put("spring.datasource.url", databaseUrl);
+                    properties.put("spring.datasource.username", databaseUsername);
+                    properties.put("spring.datasource.password", password);
+                } catch (JsonProcessingException e) {
+                    log.error("{}", e.getMessage(), e);
+                } catch (IOException e) {
+                    log.error("{}", e.getMessage(), e);
+                }
 
-            String url = environment.getProperty("azure.keyvault.url");
-            if (url == null) {
-                // 从远程vault这种管理工具或者redis里面获取配置信息，然后再更新到本地的配置中 https://learn.microsoft.com/zh-cn/azure/developer/java/spring-framework/configure-spring-boot-starter-java-app-with-azure-key-vault
-                properties.put("password", "vault获取的值");
-            }
-            PropertiesPropertySource propertySource = new PropertiesPropertySource(currentProfile, properties);
+                String url = environment.getProperty("azure.keyvault.url");
+                if (url == null) {
+                    // 从远程vault这种管理工具或者redis里面获取配置信息，然后再更新到本地的配置中 https://learn.microsoft.com/zh-cn/azure/developer/java/spring-framework/configure-spring-boot-starter-java-app-with-azure-key-vault
+                    properties.put("password", "vault获取的值");
+                }
+
+                PropertiesPropertySource propertySource = new PropertiesPropertySource(currentProfile, properties);
 //            PropertiesPropertySource propertySource = new PropertiesPropertySource("prod", properties); // 只修改指定环境变量的配置信息
-            environment.getPropertySources().addFirst(propertySource);
+                environment.getPropertySources().addFirst(propertySource);
+
+            }
         }
     }
 }
