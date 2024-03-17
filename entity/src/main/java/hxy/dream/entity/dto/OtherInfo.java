eb2740cfde6d09f5cb97e4b012c9e010ee9c0e36
捕风的逍遥侯 <aohanhongzhi@qq.com>
2024-03-17 21:48:14 +0800

    对于 Java 中的记录（Record），你无法直接使用构造函数或公共方法来为其字段赋值，因为记录是不可变的，其字段是 final 的，并且由编译器自动生成 getter 方法。要创建并设置记录的实例，可以使用记录的构造函数。

--- entity/src/main/java/hxy/dream/entity/dto/OtherInfo.java
+++ entity/src/main/java/hxy/dream/entity/dto/OtherInfo.java
@@ -2,7 +2,13 @@ package hxy.dream.entity.dto;
 
 import lombok.Data;
 
-// 貌似没有setter方法
+/**
+ * 对于 Java 中的记录（Record），你无法直接使用构造函数或公共方法来为其字段赋值，因为记录是不可变的，其字段是 final 的，并且由编译器自动生成 getter 方法。要创建并设置记录的实例，可以使用记录的构造函数。
+ * 貌似没有setter方法
+ *
+ * @param sex
+ * @param city
+ */
 public record OtherInfo(String sex, String city) {
 
 }
