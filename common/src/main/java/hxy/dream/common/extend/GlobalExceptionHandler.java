b4616c0de04b058aa34d2f2b6aea0d41240a8683
捕风的逍遥侯 <aohanhongzhi@qq.com>
2024-05-08 13:21:48 +0800

    format

--- common/src/main/java/hxy/dream/common/extend/GlobalExceptionHandler.java
+++ common/src/main/java/hxy/dream/common/extend/GlobalExceptionHandler.java
@@ -33,8 +33,8 @@ import java.io.IOException;
 import java.util.Map;
 
 /**
- * Description:全局异常处理，采用@RestControllerAdvice + @ExceptionHandler解决
- * <br>自定义异常处理类
+ * Description:全局异常处理，采用@RestControllerAdvice + @ExceptionHandler解决 <br>
+ * 自定义异常处理类
  *
  * @author eric
  */
@@ -42,146 +42,144 @@ import java.util.Map;
 @Slf4j
 public class GlobalExceptionHandler implements InitializingBean {
 
-    /**
-     * Hide exception field in the return object
-     *
-     * @return
-     */
-    @Bean
-    @Profile("prod")
-    public ErrorAttributes errorAttributes() {
-        return new DefaultErrorAttributes() {
-            @Override
-            public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
-                return super.getErrorAttributes(webRequest, ErrorAttributeOptions.defaults().excluding(ErrorAttributeOptions.Include.EXCEPTION));
-            }
-        };
-    }
+	/**
+	 * Hide exception field in the return object
+	 *
+	 * @return
+	 */
+	@Bean
+	@Profile("prod")
+	public ErrorAttributes errorAttributes() {
+		return new DefaultErrorAttributes() {
+			@Override
+			public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
+				return super.getErrorAttributes(webRequest,
+						ErrorAttributeOptions.defaults().excluding(ErrorAttributeOptions.Include.EXCEPTION));
+			}
+		};
+	}
 
-    @ExceptionHandler(WebException.class)
-    public void handleCustomException(HttpServletResponse res, WebException ex) throws IOException {
-        // 这里可以设置status，但是还不能设置 status code
-        res.sendError(ex.getHttpStatus().value(), ex.getMessage());
-    }
+	@ExceptionHandler(WebException.class)
+	public void handleCustomException(HttpServletResponse res, WebException ex) throws IOException {
+		// 这里可以设置status，但是还不能设置 status code
+		res.sendError(ex.getHttpStatus().value(), ex.getMessage());
+	}
 
+	/**
+	 * 参数校验异常捕获 包括各种自定义的参数异常
+	 *
+	 * @param request
+	 * @param e
+	 * @param <T>fsFileId
+	 * @return
+	 */
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(ConstraintViolationException.class)
+	public <T> BaseResponseVO<?> constraintViolationExceptionHandler(HttpServletRequest request,
+			ConstraintViolationException e) {
+		log.warn("\n====>{} Exception Message: {}", request.getRequestURI(), e.getMessage(), e);
+		return BaseResponseVO.error("参数错误", e.getMessage());
+	}
 
-    /**
-     * 参数校验异常捕获 包括各种自定义的参数异常
-     *
-     * @param request
-     * @param e
-     * @param <T>fsFileId
-     * @return
-     */
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(ConstraintViolationException.class)
-    public <T> BaseResponseVO<?> constraintViolationExceptionHandler(HttpServletRequest request, ConstraintViolationException e) {
-        log.warn("\n====>{} Exception Message: {}", request.getRequestURI(), e.getMessage(), e);
-        return BaseResponseVO.error("参数错误", e.getMessage());
-    }
+	/**
+	 * 处理异常的访问链接
+	 *
+	 * @param e
+	 * @param webRequest
+	 * @return
+	 */
+	@ExceptionHandler(value = { NoHandlerFoundException.class })
+	public ModelAndView noMappingHandler(Exception e, WebRequest webRequest) {
+		log.error("No mapping exception:{}", e.getMessage(), e);
+		ModelAndView modelAndView = new ModelAndView(new MappingJackson2JsonView());
+		modelAndView.addObject("code", 404);
+		modelAndView.addObject("msg", "接口异常，详情查询服务端");
+		modelAndView.addObject("data", e.getMessage());
+		return modelAndView;
+	}
 
-    /**
-     * 处理异常的访问链接
-     *
-     * @param e
-     * @param webRequest
-     * @return
-     */
-    @ExceptionHandler(value = {NoHandlerFoundException.class})
-    public ModelAndView noMappingHandler(Exception e, WebRequest webRequest) {
-        log.error("No mapping exception:{}", e.getMessage(), e);
-        ModelAndView modelAndView = new ModelAndView(new MappingJackson2JsonView());
-        modelAndView.addObject("code", 404);
-        modelAndView.addObject("msg", "接口异常，详情查询服务端");
-        modelAndView.addObject("data", e.getMessage());
-        return modelAndView;
-    }
+	/**
+	 * 方法参数校验 https://blog.csdn.net/chengliqu4475/article/details/100834090
+	 */
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public <T> BaseResponseVO<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+		log.warn("{}", e.getMessage(), e);
+		return BaseResponseVO.error("参数检验出错啦！", e.getBindingResult().getFieldError().getDefaultMessage());
+	}
 
-    /**
-     * 方法参数校验
-     * https://blog.csdn.net/chengliqu4475/article/details/100834090
-     */
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public <T> BaseResponseVO<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        log.warn(e.getMessage(), e);
-        return BaseResponseVO.error("参数检验出错啦！", e.getBindingResult().getFieldError().getDefaultMessage());
-    }
+	/**
+	 * 处理400参数错误
+	 *
+	 * @param e
+	 * @return
+	 */
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public BaseResponseVO handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+		log.warn("参数解析失败{}", e.getMessage(), e);
+		return BaseResponseVO.badrequest("参数解析失败", e.getMessage());
+	}
 
+	/**
+	 * 405错误方法不支持
+	 *
+	 * @param e
+	 * @return
+	 */
+	@ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	public BaseResponseVO handleHttpRequestMethodNotSupportedException(HttpServletRequest request,
+			HttpRequestMethodNotSupportedException e) {
+		log.warn("\n====>[{}]不支持当前[{}]请求方法,应该是[{},{}]", request.getRequestURI(), e.getMethod(),
+				e.getSupportedHttpMethods(), e.getSupportedMethods(), e);
+		return BaseResponseVO.badrequest("请求方法不支持", e.getMessage());
+	}
 
-    /**
-     * 处理400参数错误
-     *
-     * @param e
-     * @return
-     */
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    public BaseResponseVO handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
-        log.warn("参数解析失败{}", e.getMessage(), e);
-        return BaseResponseVO.badrequest("参数解析失败", e.getMessage());
-    }
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MissingServletRequestParameterException.class)
+	public BaseResponseVO MissingServletRequestParameterException(MissingServletRequestParameterException e) {
+		log.warn("\n====>请求参数错误{}", e.getMessage(), e);
+		return BaseResponseVO.badrequest("请求参数错误", e.getMessage());
+	}
 
-    /**
-     * 405错误方法不支持
-     *
-     * @param e
-     * @return
-     */
-    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
-    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public BaseResponseVO handleHttpRequestMethodNotSupportedException(HttpServletRequest request, HttpRequestMethodNotSupportedException e) {
-        log.warn("\n====>[{}]不支持当前[{}]请求方法,应该是[{},{}]", request.getRequestURI(), e.getMethod(), e.getSupportedHttpMethods(), e.getSupportedMethods(), e);
-        return BaseResponseVO.badrequest("请求方法不支持", e.getMessage());
-    }
+	@ExceptionHandler(DuplicateKeyException.class)
+	public BaseResponseVO handleDuplicateKeyException(DuplicateKeyException e) {
+		log.warn("\n====>", e);
+		return BaseResponseVO.badrequest("请求参数错误", e.getMessage());
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MissingServletRequestParameterException.class)
-    public BaseResponseVO MissingServletRequestParameterException(MissingServletRequestParameterException e) {
-        log.warn("\n====>请求参数错误{}", e.getMessage(), e);
-        return BaseResponseVO.badrequest("请求参数错误", e.getMessage());
-    }
+	}
 
-    @ExceptionHandler(DuplicateKeyException.class)
-    public BaseResponseVO handleDuplicateKeyException(DuplicateKeyException e) {
-        log.warn("\n====>", e);
-        return BaseResponseVO.badrequest("请求参数错误", e.getMessage());
+	/**
+	 * 这个应该放在最下面比较好，最后加载 处理未定义的其他异常信息 参数为空等
+	 *
+	 * @param request
+	 * @param exception
+	 * @return
+	 */
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler(value = Exception.class)
+	public BaseResponseVO exceptionHandler(HttpServletRequest request, Exception exception) {
+		String message = "系统发生错误";
+		log.error("当前URL={} ", request.getRequestURI(), exception);
 
-    }
+		if (exception instanceof NullPointerException) {
+			message = "biu，踩雷啦！";
+		} else if (exception instanceof ValidationException) {
+			message = "参数检验出错啦！";
+		} else if (exception instanceof BaseException) {
+			message = "业务处理发生错误";
+		}
 
-    /**
-     * 这个应该放在最下面比较好，最后加载
-     * 处理未定义的其他异常信息
-     * 参数为空等
-     *
-     * @param request
-     * @param exception
-     * @return
-     */
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler(value = Exception.class)
-    public BaseResponseVO exceptionHandler(HttpServletRequest request, Exception exception) {
-        String message = "系统发生错误";
-        log.error("当前URL={} ", request.getRequestURI(), exception);
+		return BaseResponseVO.error(message, exception.getMessage());
+	}
 
-        if (exception instanceof NullPointerException) {
-            message = "biu，踩雷啦！";
-        } else if (exception instanceof ValidationException) {
-            message = "参数检验出错啦！";
-        } else if (exception instanceof BaseException) {
-            message = "业务处理发生错误";
-        }
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		log.info("\n====>全局异常注入正常");
+	}
 
-        return BaseResponseVO.error(message, exception.getMessage());
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        log.info("\n====>全局异常注入正常");
-    }
-
-    @PostConstruct
-    public void init() {
-        log.info("\n====>@PostConstruct 全局异常注入正常");
-    }
+	@PostConstruct
+	public void init() {
+		log.info("\n====>@PostConstruct 全局异常注入正常");
+	}
 }
-
