8e49d4d6a0319c09364ae689fca672323c262dc1
捕风的逍遥侯 <aohanhongzhi@qq.com>
2024-07-19 11:55:43 +0800

    加密查询得写sql

--- dao/src/main/java/hxy/dream/dao/mapper/UserMapper.java
+++ dao/src/main/java/hxy/dream/dao/mapper/UserMapper.java
@@ -2,6 +2,11 @@ package hxy.dream.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import hxy.dream.dao.model.UserModel;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * @author iris
@@ -17,5 +22,13 @@ public interface UserMapper extends BaseMapper<UserModel> {
     int updateWithoutLogicDelete(UserModel userModel);
 
     UserModel selectUserModel(UserModel userModel);
+
+
+    @Results(id = "resultMap", value = {
+            @Result(column = "address", property = "address", typeHandler = hxy.dream.dao.configuration.mybatis.CustomTypeHandler.class),
+            @Result(column = "phone_number", property = "phoneNumber", typeHandler = hxy.dream.dao.configuration.mybatis.CustomTypeHandler.class)
+    })
+    @Select("select * from user_model where phone_number = #{phoneNumber} or phone_number= #{phoneNumber, typeHandler=hxy.dream.dao.configuration.mybatis.CustomTypeHandler}")
+    List<UserModel> listByNumber(UserModel userModel);
 
 }
